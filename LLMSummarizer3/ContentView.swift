46f454e232c776071742ebc3ae1797ea7b2f8dea
Hoyados <h_ohshida@icloud.com>
2025-09-07 17:05:11 +0900

    Initial Commit

---
+++ LLMSummarizer3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LLMSummarizer3
+//
+//  Created by 大志田洋輝 on 2025/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LLMSummarizer3/LLMSummarizer3App.swift
@@ -0,0 +1,17 @@
+//
+//  LLMSummarizer3App.swift
+//  LLMSummarizer3
+//
+//  Created by 大志田洋輝 on 2025/09/07.
+//
+
+import SwiftUI
+
+@main
+struct LLMSummarizer3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
